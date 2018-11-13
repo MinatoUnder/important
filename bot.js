29a217c34ac13c92391e00208ae70906b2f9b982
MinatoUnder <44467517+MinatoUnder@users.noreply.github.com>
2018-11-13 17:53:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -227,5 +227,32 @@ client.on('message', message => {
 }
 });
 
+client.on('message', message => {
+	var prefix = "$";
+if (message.content.startsWith(prefix + 'tag')) {
+    let args = message.content.split(" ").slice(1);
+if(!args[0]) return message.reply(' **مرجو كتابة نص الدي تريد** ');  
+
+    figlet(args.join(" "), (err, data) => {
+              message.channel.send("```" + data + "```")
+           })
+}
+});
+
+
+client.on('message', message => {
+  var prefix ="$"; 
+if (message.content.startsWith(prefix + 'perms')) {
+         if(!message.channel.guild) return;
+         var perms = JSON.stringify(message.channel.permissionsFor(message.author).serialize(), null, 4);
+         var zPeRms = new Discord.RichEmbed()
+         .setColor('RANDOM')
+         .setTitle(':tools: **Permissions** ')
+         .addField(' 🎇 **Your Permissions:**',perms)
+                  message.channel.send({embed:zPeRms});
+
+    }
+});
+
 
 client.login(process.env.BOT_TOKEN);
