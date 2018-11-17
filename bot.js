71e39bc62f24b47e92fb2bac498fad124317f257
MinatoUnder <44467517+MinatoUnder@users.noreply.github.com>
2018-11-17 22:09:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -191,10 +191,11 @@ client.on('message', message => {
       message.guild.members.forEach(m => {
     
     var bc = new
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
   message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
  m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
       })
       })
       reaction2.on("collect", r => {
