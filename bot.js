d9a548023e5205c5dbb100c00084b33753e9bdb7
MinatoUnder <44467517+MinatoUnder@users.noreply.github.com>
2018-11-19 20:33:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ const stripIndents = require('common-tags').stripIndents;
 const figlet = require('figlet');
 const client = new Discord.Client();
 const prefix = '$'
-const myID = "415595760990552065,395704557293797377";
+const myID = "415595760990552065";
 ti={}  
 spee={};
 
@@ -1253,31 +1253,29 @@ client.on('message', message => {
     }
   });
 
-
 const adminprefix = "=";
-const devs = ['415595760990552065,395704557293797377'];
+const devs = ['415595760990552065','395704557293797377'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
     
 if (message.content.startsWith(adminprefix + 'setgame')) {
   client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} My game is now **`)
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
 } else 
   if (message.content.startsWith(adminprefix + 'setname')) {
 client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : The bot name is changed to`)
-return message.reply("**You can not change the name. You must stay for two hours . **");
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
 } else
   if (message.content.startsWith(adminprefix + 'setavatar')) {
 client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : Done look at my new good avatar :sunglasses:`);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
       } else     
 if (message.content.startsWith(adminprefix + 'setT')) {
   client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**My twitch is now :  ${argresult}**`)
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
 }
-
 });
 
 
